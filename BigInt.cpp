19043970b7f1bd73544ad6cfcdeeb860cf4fc774
Ahmad Beirkdar <ahmadb@protonmail.com>
2020-10-30 12:26:02 -0400

    Intial commit of files and BigInt class

---
+++ BigInt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ahmad Beirkdar on 2020-10-30.
+//
+
+#include "BigInt.h"
---
+++ BigInt.h
@@ -0,0 +1,16 @@
+//
+// Created by Ahmad Beirkdar on 2020-10-30.
+//
+
+#ifndef SIDH_CPP_BIGINT_H
+#define SIDH_CPP_BIGINT_H
+
+
+class BigInt {
+
+
+
+};
+
+
+#endif //SIDH_CPP_BIGINT_H
