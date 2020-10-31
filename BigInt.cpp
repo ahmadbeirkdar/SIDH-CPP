650d94c9bd7fd0af116e0af12ddb8ceb23c49f90
Ahmad Beirkdar <a@ahmad.ltd>
2020-10-30 22:42:22 -0400

    compile time P343_mag intialization

--- BigInt.cpp
+++ BigInt.cpp
@@ -4,7 +4,7 @@
 
 #include "BigInt.h"
 
-static const unsigned long* P343_mag = BigInt::construct_prime(BigInt::Prime::P343);
+//static const unsigned long* P434_mag = BigInt::construct_prime(BigInt::Prime::P434);
 
 BigInt::BigInt(unsigned long *mag, BigInt::Prime prime)
     : mag(mag), prime(prime),length(sizeof(mag)/mag[0])
@@ -12,17 +12,12 @@ BigInt::BigInt(unsigned long *mag, BigInt::Prime prime)
 
 }
 
-const unsigned long* BigInt::construct_prime(BigInt::Prime prime) {
-    switch (prime) {
-
-        case P343:
-            switch (machine_word) {
-                case 64:
-                    const static unsigned long temp[7] = {0xFFFFFFFFFFFFFFFF,0xFFFFFFFFFFFFFFFF,0xFFFFFFFFFFFFFFFF,0xFDC1767AE2FFFFFF,0x7BC65C783158AEA3,0x6CFC5FD681C52056,0x0002341F27177344};
-                    return temp;
-                case 32:
-                    return nullptr;
-
-            }
-    }
-}
+//constexpr std::array<unsigned long,Ceil(434/machine_word)>  BigInt::construct_prime_P434() {
+//    switch (machine_word) {
+//        case 64:
+//            std::array<unsigned long,Ceil(434/machine_word)> temp = {0xFFFFFFFFFFFFFFFF,0xFFFFFFFFFFFFFFFF,0xFFFFFFFFFFFFFFFF,0xFDC1767AE2FFFFFF,0x7BC65C783158AEA3,0x6CFC5FD681C52056,0x0002341F27177344};
+//            return temp;
+//
+//
+//    }
+//}
--- BigInt.h
+++ BigInt.h
@@ -12,27 +12,50 @@
 #include <array>
 #include <initializer_list>
 
+constexpr static const size_t machine_word = (int)sizeof(long)*CHAR_BIT;
+
+constexpr unsigned long Ceil(double num) {
+    auto inum = (unsigned long)num;
+
+    return inum + (((num > 0 )) ? 1 : 0);
+}
+
+
 class BigInt {
 
 public:
-    enum Prime {P343};
+    enum Prime {P434};
 
     BigInt()=default;
-    explicit BigInt(unsigned long *mag, BigInt::Prime prime = BigInt::Prime::P343);
+    explicit BigInt(unsigned long *mag, BigInt::Prime prime = BigInt::Prime::P434);
 
 
-    static const unsigned long* construct_prime(BigInt::Prime prime);
+//    static constexpr std::array<unsigned long,Ceil(434/machine_word)> construct_prime_P434();
 
-private:
+//private:
     unsigned long *mag;
     int length;
     Prime prime;
 
 
+    static constexpr std::array<unsigned long,Ceil(434/machine_word)> P434_mag = [] {
+        switch (machine_word) {
+            case 64:
+                std::array<unsigned long, 7> temp = {0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF,
+                                                                            0xFFFFFFFFFFFFFFFF, 0xFDC1767AE2FFFFFF,
+                                                                            0x7BC65C783158AEA3, 0x6CFC5FD681C52056,
+                                                                            0x0002341F27177344};
+                return temp;
+//            case 8:
+//                std::array<unsigned long, Ceil(434 / 8)> temp = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+//                0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xE2,
+//                0x7A, 0x76, 0xC1, 0xFD, 0xA3, 0xAE, 0x58, 0x31, 0x78, 0x5C, 0xC6, 0x7B, 0x56, 0x20,
+//                0xC5, 0x81, 0xD6, 0x5F, 0xFC, 0x6C, 0x44, 0x73, 0x17, 0x27, 0x1F, 0x34, 0x02}
 
-    constexpr static const size_t machine_word = (int)sizeof(long)*CHAR_BIT;
 
-//    static constexpr unsigned long *P343_mag;
+
+        }
+    }();
 
 
 };
