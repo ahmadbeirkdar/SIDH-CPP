8a4c3c0ed1ed082fbc139779da7eb032eb1c3be2
Ahmad Beirkdar <ahmadb@protonmail.com>
2020-11-27 22:13:17 -0500

    Removed constexpr, due to dynamic array allocation

--- BigInt.h
+++ BigInt.h
@@ -23,7 +23,7 @@ public:
     constexpr explicit BigInt(T *mag, BigInt::Prime prime = BigInt::Prime::P434)
             : mag(mag), prime(prime),length(sizeof(mag)/mag[0]) {};
 
-    constexpr explicit BigInt(std::string_view num, BigInt::Prime prime = BigInt::Prime::P434)
+    explicit BigInt(std::string_view num, BigInt::Prime prime = BigInt::Prime::P434)
     : prime(prime), length(Ceil((num.length() - 2)/(2*sizeof(T))))
     {
             if(num.substr(0,2) != "0x")
@@ -45,7 +45,7 @@ public:
 
 public:
     T *mag;
-    int length;
+    int length{};
     Prime prime;
 
 
