e665c34f5899cb21ce7535299d286ea1eee2ff64
Ahmad Beirkdar <ahmadb@protonmail.com>
2021-01-05 20:55:49 -0500

    generate sub and add tests from [-2^400,2^400]

--- tests/generateTests.py
+++ tests/generateTests.py
@@ -3,19 +3,23 @@ from random import *
 def additionTests(x):
     input = open("add_inputs.txt","w")
     output = open("add_result.txt","w")
-    min = 2**25
+    min = -2**400
     max = 2**400
     for i in range(0,x):
         a = randint(min,max)
         b = randint(min,max)
         r = a + b
         input.write(f"{hex(a)}\n{hex(b)}\n")
-        output.write((f"0x{hex(r)[2:].upper()}\n"))
+        if(r>=0):
+            r =  "0x"+ hex(r)[2:].upper()
+        else:
+            r = "-0x"+ hex(r)[3:].upper()
+        output.write((f"{r}\n"))
 
 def subTests(x):
     input = open("sub_inputs.txt","w")
     output = open("sub_result.txt","w")
-    min = 2**25
+    min = -2**400
     max = 2**400
     for i in range(0,x):
         a = randint(min,max)
