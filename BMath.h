2f3adfa4a3513c95b302afae571bbda59e0cf281
Ahmad Beirkdar <ahmadb@protonmail.com>
2021-01-08 21:06:22 -0500

    BMath::MULC

--- BMath.h
+++ BMath.h
@@ -57,6 +57,29 @@ namespace BMath {
         }
     }
 
+    template <typename T>
+    inline constexpr void MULC(T &r, T &c_o, T a, T b){
+        T bh, temp;
+        T albl, albh, ahbl, ahbh, carry;
+        T mask_low = std::numeric_limits<T>::max() >> (sizeof(T)*4);
+        T mask_high = std::numeric_limits<T>::max() << (sizeof(T)*4);
+
+        albl = (a & mask_low)*(b & mask_low);
+        albh = (a & mask_low)*(b >> (sizeof(T) * 4));
+        ahbl = (a >> (sizeof(T) * 4))*(b & mask_low);
+        ahbh = (a >> (sizeof(T) * 4))*(b >> (sizeof(T) * 4));
+        r = albl & mask_low;
+
+        temp = (albl >> (sizeof(T) * 4)) + (ahbl & mask_low) + (albh & mask_low);
+        carry = temp >> (sizeof(T) * 4);
+        r ^= temp << (sizeof(T) * 4);
+
+        temp = (ahbl >> (sizeof(T) * 4)) + (albh >> (sizeof(T) * 4)) + (ahbh & mask_low) + carry;
+        c_o = temp & mask_low;
+        carry = temp & mask_high;
+        c_o ^= (ahbh & mask_high) + carry;
+    }
+
     static constexpr long hextable[] = {
             -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
             -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
