8a8dc120cfd08a4e9a889b7fdcc480945fa58e91
Ahmad Beirkdar <ahmadb@protonmail.com>
2021-01-08 22:11:47 -0500

    generate mul tests

--- tests/generateTests.py
+++ tests/generateTests.py
@@ -32,5 +32,22 @@ def subTests(x):
             r = "-0x"+ hex(r)[3:].upper()
         output.write((f"{r}\n"))
 
+def mulTest(x):
+    input = open("mul_inputs.txt","w")
+    output = open("mul_result.txt","w")
+    min = -2**40
+    max = 2**40
+    for i in range(0,x):
+        a = randint(min,max)
+        b = randint(min,max)
+        r = a * b
+        input.write(f"{hex(a)}\n{hex(b)}\n")
+        if(r>=0):
+            r =  "0x"+ hex(r)[2:].upper()
+        else:
+            r = "-0x"+ hex(r)[3:].upper()
+        output.write((f"{r}\n"))
+
 additionTests(5000)
 subTests(5000)
+mulTest(1000)
