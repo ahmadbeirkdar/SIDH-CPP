7acf149139c2c2a034ee6fad001f53017a57d282
Ahmad Beirkdar <ahmadb@protonmail.com>
2021-02-17 20:42:59 -0500

    Use unique ptr for magnitude

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "test.h"
+#include <cassert>
 int main() {
 
     objectBenchmark();
     addTests();
     primeConstructorTest();
     subTests();
+    mulTests();
 //    customTests();
     return 0;
 }
